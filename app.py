ab7916c848b5c770c96471820aa56969238cc7f8
s1rbugra <152310633+s1rbugra@users.noreply.github.com>
2023-12-26 22:29:31 +0300

    app

---
+++ app.py
@@ -0,0 +1,12 @@
+#%% banner
+banner = """
+##########################
+#      DDOS TOOL         #
+#      s1r.bugra         #
+#       GİTHUB           #
+#      s1r.bugra         #
+#      DDOS TOOL 4.0     #
+##########################
+"""
+print(banner)
+#%% DEĞİŞKENLER
